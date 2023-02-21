664afd11284df92285872fe1c3bb15854ad781a1
EddyGiusepe <eddychirinos.unac@gmail.com>
2023-02-20 22:08:28 -0300

    Testando FastAPI

--- 1_Build_a_NLP_api_using_FastAPI_and_SpaCy/main.py
+++ 1_Build_a_NLP_api_using_FastAPI_and_SpaCy/main.py
@@ -40,5 +40,17 @@ app = FastAPI(tags=['sentence'])
 class Input(BaseModel):
     sentence: str 
 
-    
 
+@app.post("/analyze_text")
+def get_text_characteristics(sentence_input: Input):
+    document = en_core_web(sentence_input.sentence)
+    output_array = []
+    for token in document:
+        output = {
+            "Index": token.i, "Token": token.text, "Tag": token.tag_, "POS": token.pos_,
+            "Dependency": token.dep_, "Lemma": token.lemma_, "Shape": token.shape_,
+            "Alpha": token.is_alpha, "Is Stop Word": token.is_stop
+        }
+        output_array.append(output)
+    return {"output": output_array}
+    
