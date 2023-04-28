160ced9a737fb9d36e3e4fddc30b30b2f2724642
EddyGiusepe <eddychirinos.unac@gmail.com>
2023-04-27 22:44:34 -0300

    Estudando o startup e shutdown no FastAPI

---
+++ 2_Introduction_to_Lifespan_Events_in_FastAPI/main_1.py
@@ -0,0 +1,8 @@
+"""
+Data Scientist.: PhD.Eddy Giusepe Chirinos Isidro
+
+
+Introdução aos eventos de tempo de vida no FastAPI
+==================================================
+Nova implementação para lógica de inicialização e desligamento
+"""
